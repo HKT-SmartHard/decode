dfe68defc1c9c3918a4a0faeba6de383ce1b0140
Auzst <77470816+Auzst@users.noreply.github.com>
2023-08-23 17:05:07 +0800

    Update sd-100_ttn.js

--- sd-100 Smoke detector/sd-100_ttn.js
+++ sd-100 Smoke detector/sd-100_ttn.js
@@ -6,55 +6,94 @@
  * @product HKT-SD-100
  */
 
+
+
 function easy_decode(bytes) {
     var decoded = {};
 
     if (checkReportSync(bytes) == false)
         return;
 
-    var status = byteToUint16Swap(bytes.slice(2, 4));
-
-    decoded.voltageAlarm = status >> 8 & 0x01;
-    decoded.tamperAlarm = status >> 4 & 0x01;
-    decoded.faultAlarm = status >> 2 & 0x01;
-    decoded.smokeAlarm = status >> 1 & 0x01;
-
-    var dataLen = bytes[4];
+    var temp;
+    var dataLen = bytes.length - 5;
     var i = 5;
     while (dataLen--) {
         var type = bytes[i];
         i++;
         switch (type) {
-            case 0x88:// battery /mv
-                decoded.battery = byteToUint16Swap(bytes.slice(i, i + 2));
+            case 0x01:  //software_ver and hardware_ver
+                decoded.hard_ver = bytes[i];
+                decoded.soft_ver = bytes[i + 1];
+                dataLen -= 2;
                 i += 2;
                 break;
-            case 0x03:// temperature /0.1℃
-                decoded.temperature = byteToUint16Swap(bytes.slice(i, i + 2)) / 10;
-                i += 2;
+            case 0x89:// BATTERY
+                decoded.battery = bytes[i];
+                dataLen -= 1;
+                i += 1;
                 break;
-            case 0xff:// report interval /min
-                decoded.reportInterval = byteToUint16Swap(bytes.slice(i, i + 2));
-                i += 2;
+            case 0x09:// TEMPERATURE
+                temp = byteToInt32(bytes.slice(i, i + 3));
+                if (temp > 0x7FFFFFFF)
+                    temp = -(temp & 0x7FFFFFFF);
+                // ℃
+                decoded.temperature = byteToInt32(bytes.slice(i, i + 3)) / 1000;
+
+                // ℉
+                // decoded.temperature = byteToInt16(bytes.slice(i, i + 3)) / 1000 * 1.8 + 32;
+
+                dataLen -= 3;
+                i += 3;
+                break;
+            case 0x27:// SMOKE_WARNING
+                decoded.smoke_warning = bytes[i];
+                dataLen -= 1;
+                i += 1;
+                break;
+            case 0x28:// TEMPERATURE_WARNING
+                decoded.temperature_warning = bytes[i];
+                dataLen -= 1;
+                i += 1;
                 break;
         }
     }
     return decoded;
 }
 
-function byteToUint16Swap(bytes) {
+
+
+function byteToUint16(bytes) {
     var value = (bytes[1] << 8) | bytes[0];
     return value;
 }
 
+function byteToInt16(bytes) {
+    var value = bytes[0] * 0xFF + bytes[1];
+    return value > 0x7fff ? value - 0x10000 : value;
+}
+
+function byteToInt32(bytes) {
+    var value = bytes[0] * 0xFF * 0xFF + bytes[1] * 0xFF + bytes[2];
+    return value > 0x7fffff ? value - 0x1000000 : value;
+}
+
+function hexToString(bytes) {
+    var value = "";
+    var arr = bytes.toString(16).split(",");
+    for (var i = 0; i < arr.length; i++) {
+        value += parseInt(arr[i]).toString(16);
+    }
+    return value;
+}
+
 function checkReportSync(bytes) {
-    if (bytes[0] == 0x01 && bytes[1] == 0x28) {
+    if (bytes[0] == 0x68 && bytes[1] == 0x6B && bytes[2] == 0x74) {
         return true;
     }
     return false;
 }
 
+
 function Decoder(bytes, port) {
     return easy_decode(bytes);
 }
-
