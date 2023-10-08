cf16fc5c1ceb730a151847f8dbad842641630a54
zlbb <zlb_manage@163.com>
2023-10-08 17:16:50 +0800

    update sw200 decode

--- sw-200 Smart Watch/sw-200_chirpstack.js
+++ sw-200 Smart Watch/sw-200_chirpstack.js
@@ -9,98 +9,103 @@
 function easy_decode(bytes) {
     var decoded = {};
 
-    decide_L = bytes.length;
-    while (decide_L >= 2) {
-        if (bytes[0] === 0xBD) {
-            if (bytes[1] === 0xF9) {
-                decoded.Bat_type = readUInt8LE(bytes.slice(2, 3));
-                decoded.Bat_volt = (bytes[3] + (bytes[4] << 8));
-                decoded.Signal_type = readUInt8LE(bytes.slice(5, 6));
-                decoded.Signal_strength = (bytes[6] + (bytes[7] << 8));
-                decoded.other_type = readUInt8LE(bytes.slice(8, 9));
-                decoded.num = (bytes[9] + (bytes[10] << 8) + (bytes[11] << 16) + (bytes[12] << 24));
-                decoded.timestamp = dateFormat(bytes[13] + (bytes[14] << 8) + (bytes[15] << 16) + (bytes[16] << 24));
-                bytes.splice(1, 18);
-            }
-            else if (bytes[1] === 0x03) {
+    if (bytes[0] == 0xBD) {
+        switch (bytes[1]) {
+            case 0xF9:
+                decoded.battery_type = bytes[2];
+                decoded.battery_volt = readUInt16LE_SWP16(bytes.slice(3, 5));
+                decoded.signal_type = bytes[5];
+                decoded.signal_strength = readUInt16LE_SWP16(bytes.slice(6, 8));
+                decoded.other_type = bytes[8];
+                decoded.num = readUInt32LE_SWP32(bytes.slice(9, 13));
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(13, 17));
+                break;
+
+            case 0x03:
                 decoded.lon = readDoubleLE(bytes.slice(2, 10));
                 decoded.lat = readDoubleLE(bytes.slice(10, 18));
-                decoded.north_south = String.fromCodePoint(readUInt8LE(bytes.slice(18, 19)));
-                decoded.east_west = String.fromCodePoint(readUInt8LE(bytes.slice(19, 20)));
-                decoded.status = String.fromCodePoint(readUInt8LE(bytes.slice(20, 21)));
-                decoded.timestamp = dateFormat(bytes[21] + (bytes[22] << 8) + (bytes[23] << 16) + (bytes[24] << 24));
-                bytes.splice(1, 26);
-            }
-            else if (bytes[1] === 0xC2) {
+                decoded.north_south = String.fromCodePoint(bytes[18]);
+                decoded.east_west = String.fromCodePoint(bytes[19]);
+                decoded.status = String.fromCodePoint(bytes[20]);
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(21, 25));
+                break;
+
+            case 0x32:
+                decoded.bp_high = bytes[2];
+                decoded.bp_low = bytes[3];
+                decoded.bp_heart = bytes[4];
+                decoded.blood_oxygen = bytes[5];
+                decoded.wrist_temperature = readUInt16LE_SWP16(bytes.slice(6, 8)) / 10;
+                decoded.body_temperature = readUInt16LE_SWP16(bytes.slice(8, 10)) / 10;
+                decoded.step = readUInt32LE_SWP32(bytes.slice(10, 14));
+                decoded.battery_level = bytes[14];
+                decoded.signal_strength = bytes[15];
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(16, 20));
+                break;
+
+            case 0xC2:
                 decoded.bp_high = readUInt16LE_SWP16(bytes.slice(2, 4));
                 decoded.bp_low = readUInt16LE_SWP16(bytes.slice(4, 6));
-                decoded.Bp_heart = (bytes[6] + (bytes[7] << 8));
-                decoded.timestamp = dateFormat(bytes[8] + (bytes[9] << 8) + (bytes[10] << 16) + (bytes[11] << 24));
-                bytes.splice(1, 13);
-            }
-            else if (bytes[1] === 0xC6) {
+                decoded.bp_heart = readUInt16LE_SWP16(bytes.slice(6, 8));
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(8, 12));
+                break;
 
-                decoded.BloodOxygen = (bytes[2] + (bytes[3] << 8));
-                decoded.timestamp = dateFormat(bytes[4] + (bytes[5] << 8) + (bytes[6] << 16) + (bytes[7] << 24));
-                bytes.splice(1, 9);
-            }
-            else if (bytes[1] === 0x17) {
-                decoded.enable_1 = readUInt8LE(bytes.slice(2, 3));
-                decoded.Interval_1 = (bytes[3] + (bytes[4] << 8));
-                decoded.time_start_h_1 = readUInt8LE(bytes.slice(5, 6));
-                decoded.time_start_m_1 = readUInt8LE(bytes.slice(6, 7));
-                decoded.time_end_h_1 = readUInt8LE(bytes.slice(7, 8));
-                decoded.time_end_m_1 = readUInt8LE(bytes.slice(8, 9));
+            case 0xC6:
+                decoded.blood_oxygen = readUInt16LE_SWP16(bytes.slice(2, 4));
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(4, 8));
+                break;
+
+            case 0x17:
+                decoded.enable_1 = bytes[2];
+                decoded.Interval_1 = readUInt16LE_SWP16(bytes.slice(3, 5));
+                decoded.time_start_h_1 = bytes[5];
+                decoded.time_start_m_1 = bytes[6];
+                decoded.time_end_h_1 = bytes[7];
+                decoded.time_end_m_1 = bytes[8];
 
                 decoded.enable_2 = readUInt8LE(bytes.slice(9, 10));
-                decoded.Interval_2 = (bytes[10] + (bytes[11] << 8));
-                decoded.time_start_h_2 = readUInt8LE(bytes.slice(12, 13));
-                decoded.time_start_m_2 = readUInt8LE(bytes.slice(13, 14));
-                decoded.time_end_h_2 = readUInt8LE(bytes.slice(14, 15));
-                decoded.time_end_m_2 = readUInt8LE(bytes.slice(15, 16));
+                decoded.Interval_2 = readUInt16LE_SWP16(bytes.slice(10, 12));
+                decoded.time_start_h_2 = bytes[12];
+                decoded.time_start_m_2 = bytes[13];
+                decoded.time_end_h_2 = bytes[14];
+                decoded.time_end_m_2 = bytes[15];
 
-                decoded.enable_3 = readUInt8LE(bytes.slice(16, 17));
-                decoded.Interval_3 = (bytes[17] + (bytes[18] << 8));
-                decoded.time_start_h_3 = readUInt8LE(bytes.slice(19, 20));
-                decoded.time_start_m_3 = readUInt8LE(bytes.slice(20, 21));
-                decoded.time_end_h_3 = readUInt8LE(bytes.slice(21, 22));
-                decoded.time_end_m_3 = readUInt8LE(bytes.slice(22, 23));
+                decoded.enable_3 = bytes[16];
+                decoded.Interval_3 = readUInt16LE_SWP16(bytes.slice(17, 19));
+                decoded.time_start_h_3 = bytes[19];
+                decoded.time_start_m_3 = bytes[20];
+                decoded.time_end_h_3 = bytes[21];
+                decoded.time_end_m_3 = bytes[22];
 
-                decoded.enable_4 = readUInt8LE(bytes.slice(23, 24));
-                decoded.Interval_4 = (bytes[24] + (bytes[25] << 8));
-                decoded.time_start_h_4 = readUInt8LE(bytes.slice(26, 27));
-                decoded.time_start_m_4 = readUInt8LE(bytes.slice(27, 28));
-                decoded.time_end_h_4 = readUInt8LE(bytes.slice(28, 29));
-                decoded.time_end_m_4 = readUInt8LE(bytes.slice(29, 30));
-                bytes.splice(1, 31);
-            }
-            else if (bytes[1] === 0xB5) {
-                decoded.Status = readUInt8LE(bytes.slice(2, 3));
-                decoded.timestamp = dateFormat(bytes[3] + (bytes[4] << 8) + (bytes[5] << 16) + (bytes[6] << 24));
-                bytes.splice(1, 8);
-            }
-            else if (bytes[1] === 0xBA) {
+                decoded.enable_4 = bytes[23];
+                decoded.Interval_4 = readUInt16LE_SWP16(bytes.slice(24, 26));
+                decoded.time_start_h_4 = bytes[26];
+                decoded.time_start_m_4 = bytes[27];
+                decoded.time_end_h_4 = bytes[28];
+                decoded.time_end_m_4 = bytes[29];
+                break;
+
+            case 0xB5:
+                decoded.status = bytes[2];
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(3, 7));
+                break;
+
+            case 0xBA:
                 var N = 0;
-                decoded.with_without = readUInt8LE(bytes.slice(2, 3));
-                if (decoded.with_without === 0x00) {
+                decoded.with_without = bytes[2];
+                if (decoded.with_without == 0x00) {
                     N = 4;
-                    decoded.timestamp = dateFormat(bytes[3] + (bytes[4] << 8) + (bytes[5] << 16) + (bytes[6] << 24));
+                    decoded.timestamp = readUInt32LE_SWP32(bytes.slice(3, 7));
                 }
-                else {
-                    N = 0;
+                decoded.temperature_type = readUInt8LE(bytes.slice(3 + N, 4 + N));
+                decoded.wrist_temperature = readUInt16LE_SWP16(bytes.slice(4 + N, 6 + N)) / 10;
+                decoded.body_temperature = readUInt16LE_SWP16(bytes.slice(6 + N, 8 + N)) / 10;
+                if (decoded.temperature_type == 0x02) {
+                    decoded.environment_temperature = readUInt16LE_SWP16(bytes.slice(6 + N, 8 + N)) / 10;
                 }
-                decoded.Temp_type = readUInt8LE(bytes.slice(3 + N, 4 + N));
-                decoded.wrist_Temp = (bytes[4 + N] + (bytes[5 + N] << 8)) / 10;
-                decoded.Body_Temp = (bytes[6 + N] + (bytes[7 + N] << 8)) / 10;
-                if (decoded.Temp_type === 0x02) {
-                    decoded.environment_temperature = (bytes[8 + N] + (bytes[9 + N] << 8)) / 10;
-                    bytes.splice(1, 11 + N);
-                }
-                else {
-                    bytes.splice(1, 9 + N);
-                }
-            }
-            else if (bytes[1] === 0xD6) {
+                break;
+
+            case 0xD6:
                 decoded.Type = bytes[2];
                 decoded.group = bytes[3];
                 decoded.BLE = [];
@@ -110,11 +115,11 @@ function easy_decode(bytes) {
                 for (var group = 0; group < decoded.group; group++) { //bd d6 00 01 f7f2d964 03 0800 8504 21 0805 6305 be 0807 0605 b5c4
                     count = pack_count;
                     pack_count = bytes[8 + count * 5 + group * 5];
-                    Timestamp = readUInt32LE_SWP32(bytes.slice(4 + + count * 5 + group * 5, 8 + count * 5 + group * 5));
-                    
+                    Timestamp = readUInt32LE_SWP32(bytes.slice(4 + count * 5 + group * 5, 8 + count * 5 + group * 5));
+
                     decoded.BLE.push("PackCount: " + pack_count);
-                    decoded.BLE.push("Timestamp: "+ Timestamp);
-    
+                    decoded.BLE.push("Timestamp: " + Timestamp);
+
                     for (var pack = 0; pack < pack_count; pack++) {
                         var data = {};
                         data.Major = readUInt16LE_SWP16(bytes.slice(9 + pack * 5, 11 + pack * 5));//Major
@@ -124,27 +129,52 @@ function easy_decode(bytes) {
                     }
                 }
                 // console.table(decoded.BLE);
-            }
-            else if (bytes[1] === 0x02) {
-                decoded.Upl_warn = (bytes[3] + (bytes[2] << 8)).toString(16);//Bitfield see below
-                decoded.timestamp = dateFormat(bytes[4] + (bytes[5] << 8) + (bytes[6] << 16) + (bytes[7] << 24));
-                bytes.splice(1, 9);
-            }
+                break;
+            case 0x02:
+                decoded.upl_warn = [];
+                var warn_data = {};
+                warn_data.warn = readUInt16LE_SWP16(bytes.slice(2, 4));
+                warn_data.warn_hex = warn_data.warn.toString(16);
+                warn_data.warn_bit = warn_data.warn.toString(2);
+                if ((warn_data.warn >> 14) & 0x01) {
+                    warn_data.fall_alarm = 1;
+                }
+
+                if ((warn_data.warn >> 8) & 0x01) {
+                    warn_data.wear = 1;
+                }
+
+                if ((warn_data.warn >> 7) & 0x01) {
+                    warn_data.dis_sos = 1;
+                }
+
+                if ((warn_data.warn >> 4) & 0x01) {
+                    warn_data.wear = 0;
+                }
+
+                if ((warn_data.warn >> 2) & 0x01) {
+                    warn_data.power_off = 1;
+                }
+
+                if ((warn_data.warn >> 2) & 0x01) {
+                    warn_data.sos = 1;
+                }
+
+                if ((warn_data.warn >> 2) & 0x01) {
+                    warn_data.power_low = 1;
+                }
+                decoded.upl_warn.push(warn_data);
+                decoded.timestamp = readUInt32LE_SWP32(bytes.slice(4, 8));
+                break;
+            default:
+                break;
+
         }
-        else if (bytes[0] === 0xFF) {
-            if (bytes[1] === 0x00 && bytes[2] === 0xFF) {
-                decoded.time_request = 1;
-            }
-            else if (bytes[1] === 0x10 && bytes[9] === 0xFF) {
-                decoded.Year = (bytes[3] + (bytes[2] << 8)).toString(16);//Year
-                decoded.Month = readUInt8LE(bytes.slice(4, 5));//Month
-                decoded.Day = readUInt8LE(bytes.slice(5, 6));//Day
-                decoded.Hour = readUInt8LE(bytes.slice(6, 7));//Hour
-                decoded.Minitus = readUInt8LE(bytes.slice(7, 8));//Minitus
-                decoded.Second = readUInt8LE(bytes.slice(8, 9));//Second
-            }
+    }
+    else if (bytes[0] == 0xFF) {
+        if (bytes[1] == 0x00) {
+            decoded.time_request = 1;
         }
-        decide_L = bytes.length;
     }
     return decoded;
 }
@@ -154,10 +184,6 @@ function easy_decode(bytes) {
  ********************************************/
 function readUInt8LE(bytes) {
     return (bytes & 0xFF);
-}
-
-function readUInt8LE_SWP8(bytes) {
-    return (value & 0xFF);
 }
 
 function readInt8LE(bytes) {
@@ -218,25 +244,6 @@ function readDoubleLE(bytes) {
     return Integer + decimal;
 }
 
-function readX16LE(bytes) {
-    var value = (bytes[0] << 8) + bytes[1];
-    return (value & 0xFFFF);
-}
-
-function readX16LE_SWP32(bytes) {
-    var value = (bytes[1] << 8) + bytes[0];
-    return (value & 0xFFFF);
-}
-
-function readS16LE(bytes) {
-    var value = (bytes[0] << 8) + bytes[1];
-    return (value & 0xFFFF);
-}
-
-function readS16LE_SWP32(bytes) {
-    var value = (bytes[1] << 8) + bytes[0];
-    return (value & 0xFFFF);
-}
 
 function dateFormat(timestamp) {
     var date = new Date(timestamp * 1000);
--- tool/creat_test_hex.js
+++ tool/creat_test_hex.js
@@ -4,7 +4,7 @@ function hex_join(hexString) {
 
     var combine = ""
 
-    for (let i = 0; i < hexString.length - 1; i++) {
+    for (let i = 0; i < hexString.length-1; i += 2) {
         combine += "0x" + hexString.slice(i, i + 2).toString() + ","
     }
     combine = combine.substring(0, combine.length - 1);
@@ -13,5 +13,8 @@ function hex_join(hexString) {
 }
 
 
-var data = "12345678";
+var data = "BD02800035A0116575";
 console.log(hex_join(data));
+
+var data1 = "EF AA F8 08 CD";
+console.log(data1.replaceAll(" ",",0x"));
